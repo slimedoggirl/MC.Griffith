75b5ec044e968d335458b72cc7629475dafe5039
Mary Clare Griffith <143539014+slimedoggirl@users.noreply.github.com>
2024-04-22 00:44:01 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.myFunction = function() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+
+    var hamburger = document.getElementById("hamburger-icon");
+    hamburger.addEventListener('click', myFunction);
+});
